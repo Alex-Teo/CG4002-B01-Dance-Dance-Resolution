8eb8052c0b493ac0f8a0b070a77fcd26852c7ccd
Amir Azhar <amir97azhar@gmail.com>
2021-08-29 10:26:09 +0800

    Delete directories

--- Dashboard/Backend/routes/sensorDataRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getSensorData } = require("../controller/sensorDataController");
-
-router.get("/", getSensorData);
-
-module.exports = router;
