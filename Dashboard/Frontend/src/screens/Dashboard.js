ef727ba55780bb5e55d776a8b9bd9d9dacc5e817
Amir Azhar <amir97azhar@gmail.com>
2021-09-15 10:29:36 +0800

    Add placeholder for screen description and clock

--- Dashboard/Frontend/src/screens/Dashboard.js
+++ Dashboard/Frontend/src/screens/Dashboard.js
@@ -1,27 +1,44 @@
 import "./Dashboard.css";
 import UserCard from "../components/UserCard";
+import { useState } from "react";
 
 const MainDashboard = () => {
+
+  const []
+
   return (
-    <div className="dashboardWrapper">
-      <UserCard
-        dancerId="Dancer 1"
-        delay="Delay 1"
-        currentDance="Dance 1"
-        feedback="Feedback 1"
-      />
-      <UserCard
-        dancerId="Dancer 2"
-        delay="Delay 2"
-        currentDance="Dance 2"
-        feedback="Feedback 2"
-      />
-      <UserCard
-        dancerId="Dancer 3"
-        delay="Delay 3"
-        currentDance="Dance 3"
-        feedback="Feedback 3"
-      />
+    <div>
+      <div className="screenHeader">
+        <div className="screenInfo">
+          <h1>Dashboard</h1>
+          <h2>A closer look at your performance</h2>
+        </div>
+        <div className="dateTime">
+          <h1>Time</h1>
+          <h2>Date</h2>
+          <h2>Day</h2>
+        </div>
+      </div>
+      <div className="cardWrapper">
+        <UserCard
+          dancerId="Dancer 1"
+          delay="Delay 1"
+          currentDance="Dance 1"
+          feedback="Feedback 1"
+        />
+        <UserCard
+          dancerId="Dancer 2"
+          delay="Delay 2"
+          currentDance="Dance 2"
+          feedback="Feedback 2"
+        />
+        <UserCard
+          dancerId="Dancer 3"
+          delay="Delay 3"
+          currentDance="Dance 3"
+          feedback="Feedback 3"
+        />
+      </div>
     </div>
   );
 };
