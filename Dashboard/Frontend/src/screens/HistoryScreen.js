a5b43dbec8c9269d5e9c6b05432f88e81e8ebb6e
Amir Azhar <amir97azhar@gmail.com>
2021-10-19 14:29:11 +0800

    Axios on frontend

--- Dashboard/Frontend/src/screens/HistoryScreen.js
+++ Dashboard/Frontend/src/screens/HistoryScreen.js
@@ -1,8 +1,18 @@
 import "./History.css";
+import axios from "axios";
 
 import ScreenHeader from "../components/ScreenHeader";
+import { useState, useEffect } from "react";
 
 function HistoryScreen() {
+  const [historyData, setHistoryData] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/history").then((res) => {
+      setHistoryData(res.data);
+    });
+  }, []);
+
   return (
     <div>
       <ScreenHeader
