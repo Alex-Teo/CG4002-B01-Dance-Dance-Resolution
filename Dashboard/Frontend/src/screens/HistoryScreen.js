bbb318245137a41e05f3b9ef0bb2973d32c4a9db
Amir Azhar <amir97azhar@gmail.com>
2021-10-19 17:10:41 +0800

    Add material ui datatable

--- Dashboard/Frontend/src/screens/HistoryScreen.js
+++ Dashboard/Frontend/src/screens/HistoryScreen.js
@@ -4,12 +4,46 @@ import axios from "axios";
 import ScreenHeader from "../components/ScreenHeader";
 import { useState, useEffect } from "react";
 
+import MUIDataTable from "mui-datatables";
+
 function HistoryScreen() {
   const [historyData, setHistoryData] = useState([]);
 
+  const tableColumns = [
+    {
+      name: "date",
+      label: "Date",
+      options: {
+        filter: false,
+        sort: true,
+      },
+    },
+    {
+      name: "time",
+      label: "Time",
+      options: {
+        filter: false,
+        sort: true,
+      },
+    },
+    {
+      name: "duration",
+      label: "Duration",
+      options: {
+        filter: false,
+        sort: true,
+      },
+    },
+  ];
+
+  const tableOptions = {
+    filterType: "checkbox",
+  };
+
   useEffect(() => {
     axios.get("http://localhost:5000/api/history").then((res) => {
-      setHistoryData(res.data);
+      var result = res.data;
+      setHistoryData(result);
     });
   }, []);
 
@@ -18,6 +52,12 @@ function HistoryScreen() {
       <ScreenHeader
         screenTitle="History"
         screenDesc="A trip back to the past"
+      />
+      <MUIDataTable
+        // title={"Employee List"}
+        data={historyData}
+        columns={tableColumns}
+        options={tableOptions}
       />
     </div>
   );
