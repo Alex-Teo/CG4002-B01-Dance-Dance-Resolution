1e349f7910289869b018994b864d775b7322c76b
Amir Azhar <amir97azhar@gmail.com>
2021-10-19 12:09:37 +0800

    Add template for welcome page

--- Dashboard/Frontend/src/App.js
+++ Dashboard/Frontend/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import DashboardScreen from "./screens/DashboardScreen";
 import HistoryScreen from "./screens/HistoryScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
 
 function App() {
   return (
@@ -15,8 +16,9 @@ function App() {
         <Sidebar />
         <div className="pageContainer">
           <Switch>
-            <Route path="/dashboard" component={DashboardScreen} />
-            <Route path="/history" component={HistoryScreen} />
+            <Route exact path="/" component={WelcomeScreen} />
+            <Route exact path="/dashboard" component={DashboardScreen} />
+            <Route exact path="/history" component={HistoryScreen} />
           </Switch>
         </div>
       </Router>
---
+++ Dashboard/Frontend/src/screens/WelcomeScreen.js
@@ -0,0 +1,22 @@
+import "./WelcomeScreen.css";
+import Button from "react-bootstrap/Button";
+
+function WelcomeScreen() {
+  return (
+    <div className="welcome_wrapper">
+      <div className="welcome_text">Welcome</div>
+      <div className="start_dancing_wrapper">
+        <Button
+          className="start_dancing_button"
+          variant="primary"
+          size="lg"
+          href="/dashboard"
+        >
+          Start Dancing
+        </Button>{" "}
+      </div>
+    </div>
+  );
+}
+
+export default WelcomeScreen;
