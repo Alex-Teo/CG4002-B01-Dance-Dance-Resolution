c0e1926bea72da4769acc02219b81a1c1b6fa201
Amir Azhar <amir97azhar@gmail.com>
2021-10-29 16:32:38 +0800

    Implement manudal logout script

---
+++ Dashboard/Backend/logoutScript.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+// Import Models
+
+const ProcessedData = require("./models/ProcessedData");
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.on(
+  "error",
+  console.error.bind(console, "MongoDB: Connection error: ")
+);
+
+connection.once("open", async () => {
+  console.log("MongoDB: Connected");
+  console.log(" Sending logout request");
+  logout();
+});
+
+// Fxn importData() used for testing -> importing of dummy data
+const logout = async () => {
+  try {
+    await ProcessedData.insertMany({
+      predictedDance1: "Logout",
+      predictedDance2: "Logout",
+      predictedDance3: "Logout",
+      predictedPos: "1|2|3",
+      syncDelay: 0,
+    });
+    process.exit();
+  } catch (error) {
+    console.error(" MongoDB: Error logout request", error);
+    process.exit(1);
+  }
+};
