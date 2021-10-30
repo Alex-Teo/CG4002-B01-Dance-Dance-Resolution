90279147ce46d9504ba0c913872654b7ea987cba
Amir Azhar <amir97azhar@gmail.com>
2021-10-30 16:36:18 +0800

    Finish signin page

--- Dashboard/Frontend/src/components/SignIn.js
+++ Dashboard/Frontend/src/components/SignIn.js
@@ -1,22 +1,29 @@
 import "./SignIn.css";
 
 import Button from "react-bootstrap/Button";
+import { FaUserAlt, FaLock } from "react-icons/fa";
 
 function SignIn() {
   return (
     <form className="signin_form">
-      <input
-        className="signin_email"
-        type="text"
-        name="email"
-        placeholder="Email"
-      />
-      <input
-        className="signin_pw"
-        type="text"
-        name="password"
-        placeholder="Password"
-      />
+      <div className="input_wrapper">
+        <FaUserAlt className="signin_icon" />
+        <input
+          className="signin_email"
+          type="text"
+          name="email"
+          placeholder="Email"
+        />
+      </div>
+      <div className="input_wrapper">
+        <FaLock className="signin_icon" />
+        <input
+          className="signin_pw"
+          type="text"
+          name="password"
+          placeholder="Password"
+        />
+      </div>
 
       <input className="signin_submit" type="submit" value="Login" />
       <div className="split">
