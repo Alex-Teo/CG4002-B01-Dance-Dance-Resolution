d6f37ce7b45e8b4073fb6a8730c69c2dda0ff1b8
Amir Azhar <amir97azhar@gmail.com>
2021-10-30 13:10:17 +0800

    Create login form

--- Dashboard/Frontend/src/components/SignIn.js
+++ Dashboard/Frontend/src/components/SignIn.js
@@ -1,7 +1,32 @@
-import React from "react";
+import "./SignIn.css";
+
+import Button from "react-bootstrap/Button";
 
 function SignIn() {
-  return <div>SIGN IN</div>;
+  return (
+    <form className="signin_form">
+      <input
+        className="signin_email"
+        type="text"
+        name="email"
+        placeholder="Email"
+      />
+      <input
+        className="signin_pw"
+        type="text"
+        name="password"
+        placeholder="Password"
+      />
+
+      <input className="signin_submit" type="submit" value="Login" />
+      <div className="split">
+        <hr />
+        <div className="or">or </div>
+        <hr />
+      </div>
+      <Button className="goto_reg">Register</Button>
+    </form>
+  );
 }
 
 export default SignIn;
