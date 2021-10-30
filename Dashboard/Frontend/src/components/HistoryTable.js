f830658ca6efdacf004bb65a5577ea4b33fa04dc
Amir Azhar <amir97azhar@gmail.com>
2021-10-30 20:07:24 +0800

    Add more analytics in history table

--- Dashboard/Frontend/src/components/HistoryTable.js
+++ Dashboard/Frontend/src/components/HistoryTable.js
@@ -10,7 +10,34 @@ function HistoryTable() {
   const [historyData, setHistoryData] = useState([]);
   const [modal, setModal] = useState(false);
   const [modalRes, setModalRes] = useState([]);
-  const [processedData, setProcessedData] = useState([]);
+  const [dances, setDances] = useState([]);
+  const [uniqueDances, setUniqueDances] = useState([]);
+  const [aveSyncDelay, setAveSyncDelay] = useState([]);
+
+  function mode(array) {
+    if (array.length == 0) return null;
+    var modeMap = {};
+    var maxEl = array[0],
+      maxCount = 1;
+    for (var i = 0; i < array.length; i++) {
+      var el = array[i];
+      if (modeMap[el] == null) modeMap[el] = 1;
+      else modeMap[el]++;
+      if (modeMap[el] > maxCount) {
+        maxEl = el;
+        maxCount = modeMap[el];
+      }
+    }
+    return maxEl;
+  }
+
+  function aveSync(array) {
+    var sum = 0;
+    array.forEach(function (item) {
+      sum += Number(item.syncDelay);
+    });
+    return sum.toFixed(2);
+  }
 
   const handleRowClick = (rowData, rowMeta) => {
     var id = rowData[0];
@@ -18,12 +45,13 @@ function HistoryTable() {
       toggleModal();
       console.log(res.data[0]);
       setModalRes(res.data[0]);
-      var uniqueDances = [
-        ...new Set(
-          res.data[0].overallProcessedData.map((item) => item.predictedDance1)
-        ),
-      ];
-      setProcessedData(uniqueDances);
+      var dancesList = res.data[0].overallProcessedData.map(
+        (item) => item.predictedDance1
+      );
+      var uniqueDancesSet = new Set(dancesList);
+      setDances(dancesList);
+      setUniqueDances(uniqueDancesSet);
+      setAveSyncDelay(aveSync(res.data[0].overallProcessedData));
     });
   };
 
@@ -113,20 +141,28 @@ function HistoryTable() {
             {modalRes.time}
           </div>
           <div className="modal_header_component">
-            <div className="sub_header">Duration</div>
+            <div className="sub_header">Session Duration</div>
             <br />
             {modalRes.duration}s
           </div>
         </div>
-        <div className="modalContent">
-          <div className="modal_header_component">
-            <div className="sub_header">
-              Total Dances ({processedData.length})
+        <div className="modal_content">
+          <div className="modal_content_1">
+            <div className="sub2_header">Most Frequent Dance</div>
+            <br />
+            {mode(dances)}
+            <div className="sub2_header">
+              Unique Dances ({uniqueDances.size})
             </div>
             <br />
-            {processedData.map((data) => {
-              return <div>{data}</div>;
+            {Array.from(uniqueDances).map((uD) => {
+              return <div>{uD}</div>;
             })}
+          </div>
+          <div className="modal_content_1">
+            <div className="sub2_header">Average Sync Delay</div>
+            <br />
+            {aveSyncDelay}s
           </div>
         </div>
       </div>
